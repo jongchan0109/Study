71189454cbd85656084cafba5688900f7373586b
jongchan0109 <123948591+jongchan0109@users.noreply.github.com>
2023-07-03 18:58:07 +0900

    Add files via upload

---
+++ 자료구조/Chapter1/LinearSearch.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int LSearch(int ar[], int len, int target);
+
+int main(void)
+{
+    int arr[] ={3,5,2,4,9};
+    int idx;
+    idx = LSearch(arr,5,4);
+
+    if(idx ==-1)
+        printf("탐색 실패\n");
+    else
+        printf("타겟 저장 인덱스: %d\n",idx);
+
+    return 0;
+}
+
+int LSearch(int ar[], int len, int target)
+{
+    int i;
+    for(i=0;i<len;i++)
+    {
+        if(ar[i]==target)
+            return i;
+    }
+    return -1;
+}
