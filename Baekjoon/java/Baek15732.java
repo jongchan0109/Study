f19133cd98df916395674aa3843a3192206b78f3
김종찬 <123948591+jongchan0109@users.noreply.github.com>
2023-10-12 13:54:19 +0900

    Update Baek15732.java

--- Baekjoon/java/Baek15732.java
+++ Baekjoon/java/Baek15732.java
@@ -1,51 +1,69 @@
 import java.io.*;
 import java.util.*;
 
-class Rule
-{
-    int start,end,add;
+class Rule {
+    long A, B, C;
 
-    public Rule(int start, int end, int add)
-    {
-        this.start = start;
-        this.end = end;
-        this.add = add;
+    public Rule(long A, long B, long C) {
+        this.A = A;
+        this.B = B;
+        this.C = C;
     }
 }
 
 public class Baek15732 {
-    static int N,K,D;
-    public static void main(String args[]) throws IOException{
+    static long N, K, D;
+    static Rule[] rules;
+
+    public static void main(String args[]) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        D = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N+1];
-        Rule[] rules = new Rule[K];
+        N = Long.parseLong(st.nextToken());
+        K = Long.parseLong(st.nextToken());
+        D = Long.parseLong(st.nextToken());
 
-        for(int i=0;i<K;i++)
-        {
+        rules = new Rule[(int) K];
+        long maxEnd = 0;
+
+        for (int i = 0; i < K; i++) {
             st = new StringTokenizer(bf.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int add = Integer.parseInt(st.nextToken());
+            long A = Long.parseLong(st.nextToken());
+            long B = Long.parseLong(st.nextToken());
+            long C = Long.parseLong(st.nextToken());
 
-            rules[i] = new Rule(start,end,add);
+            rules[i] = new Rule(A, B, C);
+            maxEnd = Math.max(maxEnd, B);
         }
 
+        long left = 1;
+        long right = maxEnd;
+        long result = 0;
 
-        int result = 0;
-        int max = 0;
-        for(int i=1;i<=N;i++)
-        {
-            result += arr[i];
-            if(result>=D) {
-                max = i;
-                break;
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            long totalD = 0;
+
+            for (int i = 0; i < K; i++) {
+                Rule rule = rules[i];
+                long start = rule.A;
+                long end = rule.B;
+                long gap = rule.C;
+                if (mid < start) continue;
+                long boxes = Math.min(mid, end) - start;
+                if (boxes < 0) continue;
+                long num = 1 + boxes / gap;
+                totalD += num;
+            }
+
+            if (totalD >= D) {
+                result = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
-        System.out.println(max);
+
+        System.out.println(result);
     }
 }
